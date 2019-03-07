97042a75908b4a60acec56d385bd2495d75c01cf
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-03-07 22:27:51 +0100

    Reenviar Verificación. Antes TODO Login

--- app/Http/Controllers/v1/Auth/RegisterUnverifiedController.php
+++ app/Http/Controllers/v1/Auth/RegisterUnverifiedController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Mail\Auth\VerifyAccount;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 
 class RegisterUnverifiedController extends Controller
@@ -24,12 +25,23 @@ class RegisterUnverifiedController extends Controller
     /**
      * Single Action Controller.
      *
-     * @param Request $request
 	 * @return \Illuminate\Http\JsonResponse
      */
 
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        $user = Auth::user();
+
+        if ($user->account_verified == true) {
+            return response()->json([
+                'message' => 'This account has already been verified.'
+            ], 404);
+        }
+
+        Mail::to($user)->send(new VerifyAccount($user));
+
+        return response()->json( [
+            'message' => 'E-mail send.'
+        ], 201);
     }
 }
