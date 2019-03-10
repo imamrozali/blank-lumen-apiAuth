9bc8d3bbdd87a7845b8fe043ea9df1e4834c3e15
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-03-10 12:46:53 +0100

    Recuperar contra. Modelo
    php artisan make:model PasswordReset

--- database/migrations/2019_03_10_113126_create_password_resets_table.php
+++ database/migrations/2019_03_10_113126_create_password_resets_table.php
@@ -15,6 +15,8 @@ class CreatePasswordResetsTable extends Migration
     {
         Schema::create('password_resets', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('email')->index();
+            $table->string('token');
             $table->timestamps();
         });
     }
