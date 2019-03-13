46f39192656650dfc6defa7971ff12166fbe551f
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-03-13 20:01:30 +0100

    Mejora. Tabla para verificar datos
    La idea es disponer de una única tabla con la que verificar algunos de los datos de los usuarios, ya sea durante la recuperación de la contraseña como en la actualización de emails o teléfono

---
+++ database/migrations/2019_03_13_185438_create_user_verifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_verifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->unique()->nullable();
+            $table->string('email_two')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->string('token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_verifications');
+    }
+}
