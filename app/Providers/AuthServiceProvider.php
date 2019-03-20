de2999abbb51dcb6e90b6136008c0b8430d7ec2f
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-03-20 12:33:40 +0100

    Multiple Tokens por Cliente
    A veces es útil permitir tokens de acceso múltiple por cada cliente de concesión de contraseña. Por ejemplo, el usuario inicia sesión desde varios navegadores simultáneamente.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\User;
+use Dusterio\LumenPassport\LumenPassport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,9 +26,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Here you may define how you wish users to be authenticated for your Lumen
-        // application. The callback which receives the incoming request instance
-        // should return either a User instance or null. You're free to obtain
+        // Allowing multiple tokens per client. Sometimes it's handy to allow multiple access tokens per password grant
+        // client. Eg. user logs in from several browsers simultaneously. Currently Laravel Passport does not allow
+        // that.
+
+        LumenPassport::allowMultipleTokens();
+
+        // Here you may define how you wish users to be authenticated for your Lumen application. The callback which
+        // receives the incoming request instance should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
