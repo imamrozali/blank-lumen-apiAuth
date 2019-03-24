a616765997d7a5ecfcadfa89c44fc0e6e9780804
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-03-24 10:40:21 +0100

    Solo API Auth

---
+++ app/Http/Controllers/v1/ExampleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\v2;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
